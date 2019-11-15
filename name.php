c462550a0ded0fa3faa8c2fdaf2dc9feb395d471
2099492 <2099492@talnet.com>
2019-11-15 11:57:59 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,13 @@
+<?php
+readline() . PHP_EOL;
+
+$voornaam = ("Laurens");
+echo $voornaam . PHP_EOL;
+
+readline() . PHP_EOL;
+
+$achternaam = (" van der Bilt");
+echo $achternaam . PHP_EOL;
+
+ readline("jouw naam is:" . $voornaam . $achternaam ) . PHP_EOL;
+ ?>
